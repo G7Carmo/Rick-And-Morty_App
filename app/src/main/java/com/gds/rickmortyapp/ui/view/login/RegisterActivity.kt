1f577964f245312066e3c034fb3cf18a8143c08a
Gabriel <gcarmodev@gmail.com>
2022-05-30 23:40:54 -0300

    ajuste formulario

--- app/src/main/java/com/gds/rickmortyapp/ui/view/login/RegisterActivity.kt
+++ app/src/main/java/com/gds/rickmortyapp/ui/view/login/RegisterActivity.kt
@@ -4,24 +4,22 @@ import com.gds.rickmortyapp.data.datasource.firebase.Authenticator
 import com.gds.rickmortyapp.data.datasource.firebase.InstancesFB
 import com.gds.rickmortyapp.data.repository.AuthenticatorRepository
 import com.gds.rickmortyapp.databinding.ActivityRegisterBinding
-import com.gds.rickmortyapp.ui.view.base.BaseFactoryActivity
-import com.gds.rickmortyapp.ui.viewmodel.ViewModelFactory
-import com.gds.rickmortyapp.util.extension.nextScreen
+import com.gds.rickmortyapp.ui.view.base.BaseWithViewModelActivity
+import com.gds.rickmortyapp.ui.viewmodel.RegisterViewModel
 import com.gds.rickmortyapp.util.extension.nextScreenWithFinish
+import com.google.android.material.textfield.TextInputEditText
 
-class RegisterActivity : BaseFactoryActivity<ActivityRegisterBinding, ViewModelFactory>() {
-    private lateinit var name : String
-    private lateinit var email : String
-    private lateinit var password : String
-    private lateinit var confirmPassword : String
-    override val viewModel =
-        ViewModelFactory(
-            authRepo = AuthenticatorRepository(
-                Authenticator(
-                    InstancesFB.auth
-                )
-            )
-        )
+class RegisterActivity : BaseWithViewModelActivity<ActivityRegisterBinding, RegisterViewModel>() {
+    private lateinit var name : TextInputEditText
+    private lateinit var email : TextInputEditText
+    private lateinit var password : TextInputEditText
+    private lateinit var confirmPassword : TextInputEditText
+    override val viewModel : RegisterViewModel = getMyViewModel()
+
+    private fun getMyViewModel(): RegisterViewModel {
+        return RegisterViewModel(AuthenticatorRepository(Authenticator(InstancesFB.auth)))
+    }
+
     override fun getLayoutBinding() = ActivityRegisterBinding.inflate(layoutInflater)
 
     override fun getViewRoot() = binding.root
@@ -33,10 +31,10 @@ class RegisterActivity : BaseFactoryActivity<ActivityRegisterBinding, ViewModelF
     }
 
     private fun initViews()= with(binding) {
-        name = editInputNome.text.toString().trim()
-        email = editInputEmailCadastro.text.toString().trim()
-        password = editInputSenhaCad.text.toString().trim()
-        confirmPassword = editTextConfirmSenha.text.toString().trim()
+        name = editInputNome
+        email = editInputEmailCadastro
+        password = editInputSenhaCad
+        confirmPassword = editTextConfirmSenha
     }
 
     private fun listeners() = with(binding) {
@@ -45,12 +43,26 @@ class RegisterActivity : BaseFactoryActivity<ActivityRegisterBinding, ViewModelF
             finish()
         }
         btnRegister.setOnClickListener {
-
+            validandoCampos(name,email,password,confirmPassword)
         }
     }
+
+    private fun validandoCampos(
+        name: TextInputEditText,
+        email: TextInputEditText,
+        password: TextInputEditText,
+        confirmPassword: TextInputEditText
+    ) {
+        name.text.toString().trim().isEmpty().apply {
+            binding.textInputName.error = "vazio"
+        }
+    }
+
 
     private fun observers() {
 
     }
 
 }
+
+
